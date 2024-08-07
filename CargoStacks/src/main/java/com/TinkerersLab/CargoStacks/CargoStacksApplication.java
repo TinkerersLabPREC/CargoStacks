7cd29fc449506a71f12fc0c02f81af044846846d
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-08-07 18:31:38 +0530

    renamed project

--- CargoStacks/src/main/java/com/TinkerersLab/CargoStacks/CargoStacksApplication.java
+++ CargoStacks/src/main/java/com/TinkerersLab/CargoStacks/CargoStacksApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InventoryManagementSystemApplication {
+public class CargoStacksApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InventoryManagementSystemApplication.class, args);
+        SpringApplication.run(CargoStacksApplication.class, args);
     }
 }
